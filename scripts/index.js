36e389bbd395c5edcac68618019985d598afcfe0
Igor Bulatov <Boulatov.gosha2018@yandex.ru>
2023-03-28 21:33:05 +0300

    решить проблемму

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,100 @@
-const editButton = document.querySelector('.profile__info-edit');
-const popup = document.querySelector('.popup');
-const closeButton = document.querySelector('.popup__btn-close');
-const form = document.querySelector('.popup__form');
-const nameInput = document.querySelector('.popup__form-input_type_name');
-const jobInput = document.querySelector('.popup__form-input_type_job');
-const name = document.querySelector('.profile__info-name');
-const job = document.querySelector('.profile__info-job');
+/*const popupElement = document.querySelector(".popup");
+const popupCloseButtonElement = popupElement.querySelector(".popup__close-button");
+const popupEditButtonElement = document.querySelector(".profile__edit-button");
+const formElement = popupElement.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".popup__form-field_input_name");
+const jobInput = formElement.querySelector(".popup__form-field_input_job");
+const nameElement = document.querySelector(".profile__name");
+const jobElement = document.querySelector(".profile__profession");
 
-editButton.addEventListener('click', function () {
-    popup.classList.add('popup_is-opened');
-    nameInput.value = name.textContent;
-    jobInput.value = job.textContent;
+const openPopup = function () {
+  popupElement.classList.add("popup_is-opened");
+  nameInput.value = nameElement.textContent;
+  jobInput.value = jobElement.textContent;
+};
+
+const closePopup = function () {
+  popupElement.classList.remove("popup_is-opened");
+};
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  nameElement.textContent = nameInput.value;
+  jobElement.textContent = jobInput.value;
+  closePopup();
+};
+
+popupEditButtonElement.addEventListener("click", openPopup);
+popupCloseButtonElement.addEventListener("click", closePopup);
+formElement.addEventListener("submit", handleFormSubmit);*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*Код с сохранением значения в памяти браузера*/ 
+
+
+const $editButton = document.querySelector('.profile__edit-button');
+const $popup = document.querySelector('.popup');
+const $closeButton = document.querySelector('.popup__close-button');
+const $form = document.querySelector('.popup__form');
+const $nameInput = document.querySelector('.popup__form-field_input_name');
+const $jobInput = document.querySelector('.popup__form-field_input_job');
+const $name = document.querySelector('.profile__name');
+const $job = document.querySelector('.profile__profession');
+
+const getFormData = (el) => {
+    const formData = new FormData(el);
+    let data = {};
+
+    for (const [key, value] of formData) {
+        data[key] = value;
+    }
+
+    return data;
+}
+
+const LSsave = (data) => {
+    localStorage.setItem('lsv0', JSON.stringify(data));
+}
+
+const LSload = () => {
+    return JSON.parse(localStorage.getItem('lsv0'));
+}
+
+$editButton.addEventListener('click', function () {
+    $popup.classList.add('popup_is-opened');
+    $nameInput.value = $name.textContent;
+    $jobInput.value = $job.textContent;
 });
 
-closeButton.addEventListener('click', function () {
-    popup.classList.remove('popup_is-opened');
+$closeButton.addEventListener('click', function () {
+    $popup.classList.remove('popup_is-opened');
 });
 
-form.addEventListener('submit', function (event) {
+$form.addEventListener('submit', function (event) {
     event.preventDefault();
-    name.textContent = nameInput.value;
-    job.textContent = jobInput.value;
-    popup.classList.remove('popup_is-opened');
+    
+    LSsave(getFormData($form));
+    
+    $name.textContent = $nameInput.value;
+    $job.textContent = $jobInput.value;
+    $popup.classList.remove('popup_is-opened');
 });
+
+window.addEventListener("load", (event) => {
+    const data = LSload();
+
+    $name.textContent = data.name;
+    $job.textContent = data.job;
+});
+
+
